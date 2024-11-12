10606f1f56e5f1d148d51c5349d76771c5985bcd
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2024-11-12 11:02:40 -0300

    url base de datos variable de entorno

--- connection/connection.js
+++ connection/connection.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('postgres://u1neba9i1c8cu5:pade4c06176020db8fcbb5460fafdfb59afd874da6019c5751153ff72bb5d05c4@c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/db5drbiquadh3t');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = sequelize;
