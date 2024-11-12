f5d8c5427d0e9bb3ebd7e21389d914d9da44f309
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2024-11-12 11:10:33 -0300

    ssl comleto

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('db5drbiquadh3t', 'u1neba9i1c8cu5', 'pade4c06176020db8fcbb5460fafdfb59afd874da6019c5751153ff72bb5d05c4', {
+const sequelize = new Sequelize({
+    username: 'u1neba9i1c8cu5',
+    password: 'pade4c06176020db8fcbb5460fafdfb59afd874da6019c5751153ff72bb5d05c4',
     host: 'c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    port: 5432,
+    database: 'db5drbiquadh3t',
     dialect: 'postgres'
   });
 
